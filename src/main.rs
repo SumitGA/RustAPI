00b701fb3ad10d3bc90a7ceb95c68f7db70e452c
SumitGA <sghost33@gmail.com>
2024-10-19 13:20:14 +1100

    Add constants for response and db url

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,15 @@ struct User {
     email: String
 }
 
+const DB_URL: &str = env!("DATABASE_URL");
+
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
+
 
 #[macro_use]
 extern crate serde_derive;
+
 fn main() {
-    println!("Hello, world!");
 }
