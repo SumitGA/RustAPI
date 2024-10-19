2f63bf97111ddae82ec2b0a30941fff2d22bd7be
SumitGA <sghost33@gmail.com>
2024-10-19 13:15:37 +1100

    Add User struct and imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use postgres::Error as PostgressError;
+use postgres::{Client, NoTls};
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::env;
+
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>, 
+    name: String,
+    email: String
+}
+
+
+#[macro_use]
+extern crate serde_derive;
 fn main() {
     println!("Hello, world!");
 }
