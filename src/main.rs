1abb2b5c83891341edaf5221c6d9efe258979f80
SumitGA <sghost33@gmail.com>
2024-10-19 13:37:55 +1100

    Add get_id and get_user_request_body fn with serde_json serialization

--- src/main.rs
+++ src/main.rs
@@ -58,3 +58,13 @@ fn set_database() -> Result<(), PostgressError> {
     Ok(())
 }
 
+//Get id from request URL
+fn get_id(request: &str) -> &str {
+    request.split("/").nth(2).unwrap_or_default().split_whitespace().next().unwrap_or_default()
+}
+
+//deserialize user from reqeust body without id
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
+
