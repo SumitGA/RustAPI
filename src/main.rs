967889b94ed52f2626d9742b1f7e8ebb80999313
SumitGA <sghost33@gmail.com>
2024-10-19 13:33:43 +1100

    Add set_database function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use postgres::Error as PostgressError;
 use postgres::{Client, NoTls};
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
 use std::env;
-
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 #[derive(Serialize, Deserialize)]
 struct User {
-    id: Option<i32>, 
+    id: Option<i32>,
     name: String,
-    email: String
+    email: String,
 }
 
 const DB_URL: &str = env!("DATABASE_URL");
@@ -18,9 +17,44 @@ const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
 
-
 #[macro_use]
 extern crate serde_derive;
 
 fn main() {
+    // Set Database
+    if let Err(_) = set_database() {
+        println!("Error setting database");
+        return;
+    }
+
+    // start server and print port
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwarp();
+    println!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Unable to connect: {}", e);
+            }
+        }
+    }
 }
+
+// db setup
+fn set_database() -> Result<(), PostgressError> {
+    let mut client = Client::connect(DB_URL, NoTls)?;
+    client.batch_execute(
+        "
+         CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL, 
+            email VARCHAR NOT NULL
+         )
+    ",
+    )?;
+    Ok(())
+}
+
