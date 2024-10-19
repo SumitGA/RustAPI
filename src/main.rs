e67cd249b21f3d3044c5239fb875e88b1c27d81b
SumitGA <sghost33@gmail.com>
2024-10-19 14:08:34 +1100

    Add handler for POST and GET request for controller/handler function

--- src/main.rs
+++ src/main.rs
@@ -99,3 +99,38 @@ fn handle_client(mut stream: TcpStream) {
         Err(e) => eprintln!("Unable to read stream: {}", e),
     }
 }
+
+//handle post request
+fn handle_post_request(request: &str) -> (String, String) {
+    match(get_user_request_body(&request), Client::connect(DB_URL, NoTls)) {
+        (Ok(user), Ok(mut client)) => {
+            client
+            .execute("INSERT INTO users(name, email) VALUES($1, $2", &[&user.name, &user.email])
+            .unwrap();
+            (OK_RESPONSE.to_string(), "User created".to_string())
+        }
+        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
+    }
+}
+
+// handle get request
+fn handle_get_request(request: &str) -> (String, String) {
+    match(get_id(&request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+        (Ok(id), Ok(mut client)) => {
+            match client.query_one("SELECT * FROM users WHERE id= $1", &[&id]) {
+                Ok(row) => {
+                    let user = User {
+                        id: row.get(0), 
+                        name: row.get(1),
+                        email: row.get(2),
+                    };
+                    (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap())
+                }
+                _ => (NOT_FOUND.to_string(), "User not found".to_string()),
+            }
+        }
+        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
+    }
+}
+
+
